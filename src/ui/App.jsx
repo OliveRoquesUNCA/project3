40866f35e7f33b3e12b0235824d2d1dc3babaec6
OliveRoquesUNCA <jroques@unca.edu>
2024-04-29 07:16:50 -0400

    api returning db calls

--- src/ui/App.jsx
+++ src/ui/App.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 
-export default async function Square() {
-  const resp = await fetch("/api/v1/hello");
-  console.log(resp);
+export default function App() {
+  async function Api_calls() {
+    const resp_hello = await fetch("/api/v1/hello");
+    const hello = await resp_hello.json();
+    console.log(hello);
+    const resp_films = await fetch("/api/v1/films");
+    const films = await resp_films.json();
+    console.log(films);
+  }
+
+
+  return (
+    <>
+        <header>
+            <h1>MVC App</h1>
+        </header>
+        <main>
+            <p></p>
+        </main>
+    </>
+  );
 }
